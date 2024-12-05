22c42735cfa059f76d3a32893435fc26ee164436
pikalov-a <apikalov1979@gmail.com>
2024-12-05 12:12:49 +0300

    Update module_6_2.py

--- module_6_2.py
+++ module_6_2.py
@@ -1,55 +1,50 @@
 ##"Доступ к свойствам родителя. Переопределение свойств"#module_6_2
 ##Задача "Изменять нельзя получать":
 class Vehicle:
+    __COLOR_VARIANTS = ['white', 'yellow', 'brown', 'orange', 'violet', 'red', 'blue', 'green', 'black', 'other']
     def __init__(self, owner, __model, __color, __engine_power):
-        #        self.__COLOR_VARIANTS = __COLOR_VARIANTS
         self.owner = owner
         self.__model = __model
         self.__engine_power = __engine_power
         self.__color = __color
-        def get_model(__model):
-            return(f'Модель: {self.__model}')
 
-        def get_horsepower(__engine_power):
-            return(f'Мощность двигателя: {self.__engine_power}')
+    def get_model(self):
+        return (f'Модель: {self.__model}')
 
-        def get_color(__color):
-            return(f'Цвет: {self.__color}')
+    def get_horsepower(self):
+        return (f'Мощность двигателя: {self.__engine_power}')
 
-        def print_info():
-            print(get_model(self.__model))
-            print(get_horsepower(self.__engine_power))
-            print(get_color(self.__color))
-            print(f'Владелец: {self.owner}')
+    def get_color(self):
+        return (f'Цвет: {self.__color}')
 
-        def set_color(__color, new_color):
-            if new_color.lower in __COLOR_VARIANTS:
-                __color = new_color
-            else:
-                print(f'Нельзя сменить цвет на {new_color}')
+    def print_info(self):
+        print(self.get_model())
+        print(self.get_horsepower())
+        print(self.get_color())
+        print(f'Владелец: {self.owner}')
 
-        __COLOR_VARIANTS = ['white', 'yellow', 'brown', 'orange', 'violet', 'red', 'blue', 'green', 'black', 'other']
-#        print_info()
+
+    def set_color(self, new_color):
+        if new_color.lower() in self.__COLOR_VARIANTS:
+            self.__color = new_color
+        else:
+            print(f'Нельзя сменить цвет на {new_color}')
 
 
 class Sedan(Vehicle):
-    def __init__(self, owner, __model, __color, __engine_power):
-        super().__init__( owner, __model, __color, __engine_power)
-        super().print_info()
-    #    super(Vehicle).__init__(Vehicle)
-    #    super(Vehicle).print_info(self, owner)
-    #        self.__COLOR_VARIANTS = __COLOR_VARIANTS)
-
     __PASSENGERS_LIMIT = 5
+
+    def __init__(self, owner, model, color, engine_power):
+        super().__init__(owner, model, color, engine_power)
+        self.print_info()  ## на самом деле РАБОТАЕТ БЕЗ НЕГО!!
 
 
 # __COLOR_VARIANTS = ['white', 'yellow', 'brown', 'orange', 'violet', 'red', 'blue', 'green', 'black', 'other']
-#Sedan= Sedan ('Fedos', 'Toyota Mark II', 'blue', 500 )
-vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
+vehicle1 = Vehicle('Fedos', 'Toyota Mark II', 'blue', 500)
 # Изначальные свойства
 vehicle1.print_info()
-#Vehicle.print_info('Fedos', 'Toyota Mark II', 'blue', 500)
 # Меняем свойства (в т.ч. вызывая методы)
 vehicle1.set_color('Pink')
 vehicle1.set_color('BLACK')
 vehicle1.owner = 'Vasyok'
+vehicle1.print_info()
